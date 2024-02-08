98f7a5b061e2b820bc717cf16b125cc7c2f82c25
HyunjinKim <koujie11@gmail.com>
2024-02-09 01:14:31 +0900

    add : context 에 dummy data 이용해서 post 추가하기

--- src/context/PostContext.jsx
+++ src/context/PostContext.jsx
@@ -5,8 +5,14 @@ export const PostContext = createContext(null);
 
 const PostProvider = ({ children }) => {
   const [posts, setPosts] = useState(detailListDummyData);
+  // console.log(posts);
 
-  return <PostContext.Provider value={{ posts, setPosts }}>{children}</PostContext.Provider>;
+  /* 포스트 글 추가하기 */
+  const addPostSubmit = (newpost) => {
+    setPosts((posts) => [newpost, ...posts]);
+  };
+
+  return <PostContext.Provider value={{ posts, setPosts, addPostSubmit }}>{children}</PostContext.Provider>;
 };
 
 export default PostProvider;
