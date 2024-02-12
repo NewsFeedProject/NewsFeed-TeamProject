89e95beabffc837cf8cce3dc5a50120baf5dcd68
KY <rudyeon@naver.com>
2024-02-12 22:39:29 +0900

    Feat: 내가 작성한 글 구현

--- src/components/mypage/MyPostList.jsx
+++ src/components/mypage/MyPostList.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import { UsersContext } from "../../context/UsersContext";
+import { PostContext } from "../../context/PostContext";
+import React, { useContext } from "react";
+import { useNavigate, useParams } from "react-router";
 
 function MyPostList() {
-  return <div>MyPostList</div>;
+  const navigate = useNavigate();
+  const params = useParams();
+  const { posts } = useContext(PostContext);
+  const { users } = useContext(UsersContext);
+  const foundUser = users.find((user) => {
+    return user.uid === params.uid;
+  });
+  const filteredPost = posts.filter((post) => post.userEmail === foundUser.userEmail);
+  return (
+    <>
+      <div>MyPostList</div>
+      <div
+        onClick={() => {
+          navigate(`/detail/${post.postId}`);
+        }}
+      >
+        {filteredPost.length === 0 ? (
+          <p>면접 후기를 남겨주세요!</p>
+        ) : (
+          filteredPost.map((post) => {
+            return (
+              <div key={post.postId} style={{ border: "1px solid green", padding: "20px" }}>
+                <div style={{ border: "1px solid green", borderRadius: "10px", width: "150px", padding: "10px" }}>
+                  {post.postCategory === "interview"
+                    ? "면접후기"
+                    : post.postCategory === "workInfo"
+                    ? "취업정보"
+                    : "회사 정보 공유"}
+                </div>
+                <p>{post.postTitle}</p>
+                <p>{post.postText}</p>
+              </div>
+            );
+          })
+        )}
+      </div>
+    </>
+  );
 }
 
 export default MyPostList;
