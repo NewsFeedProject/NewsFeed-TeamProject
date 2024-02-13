56d280447aafa460246150031272d6c26969631b
HyunjinKim <koujie11@gmail.com>
2024-02-14 08:58:57 +0900

    remove : 불필요한 파일 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,8 @@ import PostProvider from "context/PostContext";
 import LoginContextProvider from "context/LoginContext";
 import SingUpContextProvider from "context/SingUpContext";
 import Router from "shared/Router";
-import { collection, getDocs } from "firebase/firestore/lite";
-import { db } from "data/firebase";
-import { useEffect } from "react";
 
 function App() {
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const querySnapshot = await getDocs(collection(db, "postInfo"));
-  //     querySnapshot.forEach((doc) => {
-  //       console.log(`${doc.id} => ${doc.data()}`);
-  //     });
-  //   };
-  //   fetchData();
-  // }, []);
   return (
     <>
       <UserProvider>
