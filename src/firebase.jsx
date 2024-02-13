c67c3b926f9b53755ed93b9e0352b9762e3852a9
HyunjinKim <koujie11@gmail.com>
2024-02-13 10:17:19 +0900

    feat : firebase 설정

---
+++ src/firebase.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCtPCqUyQpFnK6GGQr2cNsKEZYiSA7Gwf8",
+  authDomain: "newsfeed-e1d2f.firebaseapp.com",
+  projectId: "newsfeed-e1d2f",
+  storageBucket: "newsfeed-e1d2f.appspot.com",
+  messagingSenderId: "1028671899614",
+  appId: "1:1028671899614:web:67440012e33c316b607f4f"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
