d6619183678e6b659134f507afcaf0c5cd0b731d
HyunjinKim <koujie11@gmail.com>
2024-02-13 11:20:55 +0900

    test : list 수정 중

--- src/components/posts/PostList.jsx
+++ src/components/posts/PostList.jsx
@@ -4,7 +4,10 @@ import PostCards from "./PostCards";
 
 function PostList() {
   const { posts } = useContext(PostContext);
-  // const filteredPost = posts.filter((post) => post.postCategory === activePost);
+  const filteredPost = posts.filter((post) => {
+    post.postCategory === "면접 후기";
+    console.log(post.postCategory);
+  });
   return (
     <>
       <ul>
