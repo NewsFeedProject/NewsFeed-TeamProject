5c9f22a751d7748d25bf4ae2e8a925edee77daee
HyunjinKim <koujie11@gmail.com>
2024-02-14 10:45:51 +0900

    style : css 수정

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,18 +2,26 @@ import React from "react";
 import Header from "components/common/Header";
 import Navbar from "components/homePage/Navbar";
 import { Outlet } from "react-router";
-
+import styled from "styled-components";
 function Layout({ children }) {
   return (
     <>
-      <Header />
+      {/* <Header />
       <div style={{ display: "flex" }}>
         <Navbar />
         {children}
         <Outlet />
-      </div>
+      </div> */}
+      <Header />
+      <Navbar />
+      <childrenContainer>{children}</childrenContainer>
+      <Outlet />
     </>
   );
 }
 
 export default Layout;
+
+const childrenContainer = styled.div`
+  margin-top: 200px;
+`;
